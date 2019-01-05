3443174c74f449f3001067102afcf341e9f712a8
kpilszak <klaudia.pilszak@gmail.com>
2019-01-05 15:35:47 +0100

    Add poles in Player.cs

--- adventure-game-expedition/Player.cs
+++ adventure-game-expedition/Player.cs
@@ -1,7 +1,11 @@
-﻿namespace adventure_game_expedition
+﻿using System.Collections.Generic;
+
+namespace adventure_game_expedition
 {
     class Player : Mover
     {
-
+        private Weapon equippedWeapon;
+        public int HitPoints { get; private set; }
+        private List<Weapon> inventory = new List<Weapon>();
     }
 }
