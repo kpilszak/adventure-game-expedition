9507db20d8b923b88d9c1cb79558098026bc644f
kpilszak <klaudia.pilszak@gmail.com>
2019-01-05 15:23:04 +0100

    Add poles in Mover.cs

--- adventure-game-expedition/Mover.cs
+++ adventure-game-expedition/Mover.cs
@@ -1,7 +1,12 @@
-﻿namespace adventure_game_expedition
+﻿using System.Drawing;
+
+namespace adventure_game_expedition
 {
     abstract class Mover
     {
-
+        private const int MoveInterval = 10;
+        protected Point location;
+        public Point Location { get { return location; } }
+        protected Game game;
     }
 }
