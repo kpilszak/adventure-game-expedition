364a500d9d6861d1a6b3ca0bebff17792d5dec9b
kpilszak <klaudia.pilszak@gmail.com>
2019-01-05 15:10:18 +0100

    Add poles in Game.cs

--- adventure-game-expedition/Game.cs
+++ adventure-game-expedition/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace adventure_game_expedition
 {
     class Game
     {
+        public IEnumerable<Enemy> Enemies { get; private set; }
+        public Weapon WeaponInRoom { get; private set; }
+        private Player player;
+        public Point PlayerLocation { get { return player.Location; } }
+        public int PlayerHitPoints { get { return player.HitPoints; } }
+        public IEnumerable<string> PlayerWeapons { get { return player.Weapons; } }
+        private int level = 0;
+        public int Level { get { return level; } }
+        private Rectangle boundaries;
+        public Rectangle Boundaries { get { return boundaries; } }
     }
 }
