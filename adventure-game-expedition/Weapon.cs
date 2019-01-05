f091353633da898273aefe5d90c6a969176bdbab
kpilszak <klaudia.pilszak@gmail.com>
2019-01-05 18:58:43 +0100

    Add methods in Weapon.cs

--- adventure-game-expedition/Weapon.cs
+++ adventure-game-expedition/Weapon.cs
@@ -1,4 +1,7 @@
-﻿namespace adventure_game_expedition
+﻿using System;
+using System.Drawing;
+
+namespace adventure_game_expedition
 {
     abstract class Weapon : Mover
     {
@@ -13,6 +16,28 @@
         public void PickUpWeapon()
         {
             PickedUp = true;
+        }
+
+        public abstract string Name { get; }
+
+        public abstract void Attack(Direction direction, Random random);
+
+        protected bool DamageEnemy(Direction direction, int radius, int damage, Random random)
+        {
+            Point target = game.PlayerLocation;
+            for (int distance = 0; distance < radius; distance++)
+            {
+                foreach (Enemy enemy in game.Enemies)
+                {
+                    if (Nearby(enemy.Location, target, distance))
+                    {
+                        enemy.Hit(damage, random);
+                        return true;
+                    }
+                }
+                target = Move(direction, target, game.Boundaries);
+            }
+            return false;
         }
     }
 }
