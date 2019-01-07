a88c5c96e2eb95cb01b1fb65d2be39848c7de34d
kpilszak <klaudia.pilszak@gmail.com>
2019-01-07 18:11:24 +0100

    Add weapons

---
+++ adventure-game-expedition/Sword.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventure_game_expedition
+{
+    class Sword : Weapon
+    {
+        public Sword(Game game, Point location)
+            : base(game, location)
+        { }
+
+        public override string Name
+        {
+            get
+            {
+                return "Sword";
+            }
+        }
+
+        public override void Attack(Direction direction, Random random)
+        {
+            switch (direction)
+            {
+                case Direction.Up:
+                    if (!DamageEnemy(direction, 10, 3, random))
+                        if (!DamageEnemy(Direction.Right, 10, 3, random))
+                            if (!DamageEnemy(Direction.Down, 10, 3, random))
+                                break;
+                    break;
+
+                case Direction.Down:
+                    if (!DamageEnemy(direction, 10, 3, random))
+                        if (!DamageEnemy(Direction.Left, 10, 3, random))
+                            if (!DamageEnemy(Direction.Up, 10, 3, random))
+                                break;
+                    break;
+
+                case Direction.Left:
+                    if (!DamageEnemy(direction, 10, 3, random))
+                        if (!DamageEnemy(Direction.Up, 10, 3, random))
+                            if (!DamageEnemy(Direction.Right, 10, 3, random))
+                                break;
+                    break;
+
+                case Direction.Right:
+                    if (!DamageEnemy(direction, 10, 3, random))
+                        if (!DamageEnemy(Direction.Down, 10, 3, random))
+                            if (!DamageEnemy(Direction.Left, 10, 3, random))
+                                break;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
