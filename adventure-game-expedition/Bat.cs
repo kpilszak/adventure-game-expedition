7f8a3a9624e0a3804cbdef714406d72f50236977
kpilszak <klaudia.pilszak@gmail.com>
2019-01-07 17:51:42 +0100

    Add enemies

---
+++ adventure-game-expedition/Bat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventure_game_expedition
+{
+    class Bat : Enemy
+    {
+        public Bat(Game game, Point location)
+            : base(game, location, 6)
+        { }
+
+        public override void Move(Random random)
+        {
+            if (HitPoints >= 1)
+            {
+                if (random.Next(2) == 0)
+                    location = Move(FindPlayerDirection(game.PlayerLocation), game.Boundaries);
+                else
+                    location = Move((Direction)random.Next(4), game.Boundaries);
+            }
+
+            if (NearPlayer())
+                game.HitPlayer(2, random);
+        }
+    }
+}
---
+++ adventure-game-expedition/Ghoul.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventure_game_expedition
+{
+    class Ghoul : Enemy
+    {
+        public Ghoul(Game game, Point location)
+            : base(game, location, 10)
+        { }
+
+        public override void Move(Random random)
+        {
+            if (HitPoints >= 1)
+            {
+                int randValue = random.Next(3);
+                if (randValue == 0 || randValue == 1)
+                    location = Move(FindPlayerDirection(game.PlayerLocation), game.Boundaries);
+            }
+
+            if (NearPlayer())
+                game.HitPlayer(4, random);
+        }
+    }
+}
